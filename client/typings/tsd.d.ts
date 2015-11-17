f3ee497d9ab5964bbb479cb7aeafd164bb971226
ryanluker <ryanluker@outlook.com>
2015-11-16 22:02:59 -0800

    do not commit typings

--- client/typings/tsd.d.ts
+++
@@ -1,8 +0,0 @@
-
-/// <reference path="react/react.d.ts" />
-/// <reference path="webpack/webpack.d.ts" />
-/// <reference path="react/react-dom.d.ts" />
-/// <reference path="react-mixin/react-mixin.d.ts" />
-/// <reference path="react-router/react-router.d.ts" />
-/// <reference path="firebase/firebase.d.ts" />
-/// <reference path="history/history.d.ts" />
