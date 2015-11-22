d6b35a50b0e46c7161de7b2f8fa3bbd9ccb6ecbf
ryanluker <ryanluker@outlook.com>
2015-11-22 10:13:09 -0800

    cleanup index html location and add store selector

--- client/scripts/main.tsx
+++ client/scripts/main.tsx
@@ -5,11 +5,15 @@ import * as ReactDOM from 'react-dom';
  * Store Picker example
  */
 class StorePicker extends React.Component<any, any> {
-	render() {
-		return (
-			<p>hello</p>
-		)
-	}
+  render() {
+	return (
+	  <form className="store-selector">
+	  	<h2>Please Enter a Store</h2>
+		<input type="text" ref="storeId" required />
+		<input type="submit" />
+	  </form>
+	)
+  }
 }
 
 ReactDOM.render(<StorePicker/>, document.getElementById('main'));
