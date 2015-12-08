cd78f697bc8d4757ab77ebeaf7b60bd3d70f0833
ryanluker <ryanluker@outlook.com>
2015-12-07 21:49:23 -0800

    remove jsdoc as interface serves same function

--- client/scripts/helpers.ts
+++ client/scripts/helpers.ts
@@ -6,28 +6,14 @@ interface HelpersInterface {
 }
 
 class helpers implements HelpersInterface {
-  /**
-   * Takes cents and returns dollar
-   * @param {Number} cents - cents value as a number
-   * @returns {String} string of dollars based on cents
-   */
   formatPrice(cents: number) {
     return '$' + ( (cents / 100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") );
   }
-  /**
-   * Takes an array and returns a random one
-   * @param {Array<string>} arr - array of strings
-   * @returns {String} one element from the given array of strings
-   */
+
   rando(arr: Array<string>) {
     return arr[Math.floor(Math.random() * arr.length)];
   }
 
-  /**
-   * Takes a string and removes human readable bits
-   * @param {String} text - text to become simpler
-   * @returns {string} slug form of string
-   */
   slugify(text: string) {
     return text.toString().toLowerCase()
       .replace(/\s+/g, '-')           // Replace spaces with -
@@ -37,10 +23,6 @@ class helpers implements HelpersInterface {
       .replace(/-+$/, '');            // Trim - from end of text
   }
 
-  /**
-   * Uses a library of words to return a 3 word combo
-   * @returns {String} A string containing 3 random words
-   */
   getFunName() {
     var adjectives = ['adorable', 'beautiful', 'clean', 'drab', 'elegant', 'fancy', 'glamorous', 'handsome', 'long', 'magnificent', 'old-fashioned', 'plain', 'quaint', 'sparkling', 'ugliest', 'unsightly', 'angry', 'bewildered', 'clumsy', 'defeated', 'embarrassed', 'fierce', 'grumpy', 'helpless', 'itchy', 'jealous', 'lazy', 'mysterious', 'nervous', 'obnoxious', 'panicky', 'repulsive', 'scary', 'thoughtless', 'uptight', 'worried'];
 
