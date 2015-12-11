d1da70856b81464660376a31e18dc26c25d0a7ab
ryanluker <ryanluker@outlook.com>
2015-12-10 23:18:05 -0800

    cleanup onSubmit and create cleaner this binding https://github.com/Microsoft/TypeScript/issues/4530

--- client/scripts/main.tsx
+++ client/scripts/main.tsx
@@ -77,14 +77,14 @@ class Header extends React.Component<HeaderProps, any> {
  * Store Picker example
  */
 class StorePicker extends React.Component<any, any> {
-  goToStore(event: React.FormEvent) {
+  private goToStore = (event: React.FormEvent) => {
     event.preventDefault();
     let storeInput = findDOMNode<HTMLInputElement>(this.refs["storeId"]);
     this.props.history.replaceState(null, "/store/" + storeInput.value);
   }
   render() {
     return (
-      <form className="store-selector" onSubmit={e => this.goToStore(e)}>
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
         <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
         <input type="submit" />
